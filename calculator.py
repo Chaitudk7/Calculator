285beeeed51a2675ae82abc1dfcd05cb289b09f7
krishna chaitanya <139568099+Chaitudk7@users.noreply.github.com>
2025-06-26 19:25:40 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+first_num=int(input("enter first number: "))
+second_num=int(input("enter second number: "))
+addition=first_num+second_num
+subtraction=first_num-second_num
+Multiplication=first_num*second_num
+Division=first_num/second_num
+print(f"addition:{addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {Multiplication}") 
+print(f"Division: {Division}")
